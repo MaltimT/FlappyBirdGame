33232f4ed768c45ff80a2c3560aa93871d14c799
Maltim <timurm13@ukr.net>
2021-05-29 19:27:02 +0300

    Add project files.

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.tbSpeed = new System.Windows.Forms.TrackBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbDown = new System.Windows.Forms.CheckBox();
+            this.rbLeft = new System.Windows.Forms.CheckBox();
+            this.rbUp = new System.Windows.Forms.CheckBox();
+            this.rbRight = new System.Windows.Forms.CheckBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pnlControls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlControls
+            // 
+            this.pnlControls.BackColor = System.Drawing.Color.DarkOrchid;
+            this.pnlControls.Controls.Add(this.tbSpeed);
+            this.pnlControls.Controls.Add(this.btnStart);
+            this.pnlControls.Controls.Add(this.label2);
+            this.pnlControls.Controls.Add(this.label1);
+            this.pnlControls.Controls.Add(this.rbDown);
+            this.pnlControls.Controls.Add(this.rbLeft);
+            this.pnlControls.Controls.Add(this.rbUp);
+            this.pnlControls.Controls.Add(this.rbRight);
+            this.pnlControls.Location = new System.Drawing.Point(476, 35);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Size = new System.Drawing.Size(542, 551);
+            this.pnlControls.TabIndex = 0;
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.BackColor = System.Drawing.Color.DarkViolet;
+            this.tbSpeed.Location = new System.Drawing.Point(186, 281);
+            this.tbSpeed.Maximum = 40;
+            this.tbSpeed.Minimum = 20;
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.Size = new System.Drawing.Size(167, 45);
+            this.tbSpeed.TabIndex = 7;
+            this.tbSpeed.Value = 40;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.DarkViolet;
+            this.btnStart.Location = new System.Drawing.Point(186, 344);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(167, 101);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkViolet;
+            this.label2.Location = new System.Drawing.Point(200, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Choose your maximum speed";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(220, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Choose your direction";
+            // 
+            // rbDown
+            // 
+            this.rbDown.AutoSize = true;
+            this.rbDown.BackColor = System.Drawing.Color.DarkViolet;
+            this.rbDown.Location = new System.Drawing.Point(336, 165);
+            this.rbDown.Name = "rbDown";
+            this.rbDown.Size = new System.Drawing.Size(54, 17);
+            this.rbDown.TabIndex = 3;
+            this.rbDown.Text = "Down";
+            this.rbDown.UseVisualStyleBackColor = false;
+            // 
+            // rbLeft
+            // 
+            this.rbLeft.AutoSize = true;
+            this.rbLeft.BackColor = System.Drawing.Color.DarkViolet;
+            this.rbLeft.Location = new System.Drawing.Point(151, 208);
+            this.rbLeft.Name = "rbLeft";
+            this.rbLeft.Size = new System.Drawing.Size(44, 17);
+            this.rbLeft.TabIndex = 2;
+            this.rbLeft.Text = "Left";
+            this.rbLeft.UseVisualStyleBackColor = false;
+            // 
+            // rbUp
+            // 
+            this.rbUp.AutoSize = true;
+            this.rbUp.BackColor = System.Drawing.Color.DarkViolet;
+            this.rbUp.Location = new System.Drawing.Point(336, 208);
+            this.rbUp.Name = "rbUp";
+            this.rbUp.Size = new System.Drawing.Size(40, 17);
+            this.rbUp.TabIndex = 1;
+            this.rbUp.Text = "Up";
+            this.rbUp.UseVisualStyleBackColor = false;
+            // 
+            // rbRight
+            // 
+            this.rbRight.AutoSize = true;
+            this.rbRight.BackColor = System.Drawing.Color.DarkViolet;
+            this.rbRight.Location = new System.Drawing.Point(151, 165);
+            this.rbRight.Name = "rbRight";
+            this.rbRight.Size = new System.Drawing.Size(51, 17);
+            this.rbRight.TabIndex = 0;
+            this.rbRight.Text = "Right";
+            this.rbRight.UseVisualStyleBackColor = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 33;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1284, 639);
+            this.Controls.Add(this.pnlControls);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.pnlControls.ResumeLayout(false);
+            this.pnlControls.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlControls;
+        private System.Windows.Forms.TrackBar tbSpeed;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox rbDown;
+        private System.Windows.Forms.CheckBox rbLeft;
+        private System.Windows.Forms.CheckBox rbUp;
+        private System.Windows.Forms.CheckBox rbRight;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwoBallCollision;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> walls;
+        PictureBox bird, hwall1, hwall2;
+        List<int> speeds;
+        Vector r, r0, gx, gy;
+        double m;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            walls = new List<PictureBox>();
+            speeds = new List<int>();
+            m = 0.4;
+            gx = new Vector(10, 0);
+            gy = new Vector(0, 10);
+            r = new Vector(-100, -100);
+        }
+        //START BUTTON
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            pnlControls.Visible = false;
+
+            //LIMITATION OF THE COUNT OF THE DIRECTIONS
+
+            if(rbLeft.Checked && rbRight.Checked || rbLeft.Checked && rbUp.Checked 
+           || rbLeft.Checked && rbDown.Checked || rbRight.Checked && rbUp.Checked
+           || rbRight.Checked && rbDown.Checked || rbUp.Checked && rbDown.Checked 
+           || rbLeft.Checked && rbRight.Checked && rbUp.Checked && rbDown.Checked)
+            {
+                MessageBox.Show("Choose only one direction");
+                Application.Exit();
+            }
+
+            //CREATE INVISIBLE WALLS AND BIRDS
+            if (rbLeft.Checked)
+            {
+                CreateBirdLeft();
+                CreateInvisibleWallsForLeftAndRight();
+            }
+            if (rbRight.Checked)
+            {
+                CreateBirdRight();
+                CreateInvisibleWallsForLeftAndRight();
+            }
+            if (rbUp.Checked)
+            {
+                CreateBirdUp();
+                CreateInvisibleWallsForUpAndDown();
+            }
+            if (rbDown.Checked)
+            {
+                CreateBirdDown();
+                CreateInvisibleWallsForUpAndDown();
+            }
+
+            timer.Start();                     
+        }       
+
+        //TIMER
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            r0 = new Vector(bird.Location.X, bird.Location.Y);
+            //CREATE WALLS
+            if (rbRight.Checked)
+            {
+                bird.Top += (int)(gy.Y * m * 2);
+                if (rnd.Next(12) == 1)
+                {                    
+                    CreateWallsRight();
+                }
+                for (int i = 0; i < walls.Count; i++)
+                {
+                    walls[i].Left -= speeds[i];
+                }
+            }
+            if (rbLeft.Checked)
+            {
+                bird.Top += (int)(gy.Y * m * 2);
+                if (rnd.Next(12) == 1)
+                {
+                    CreateWallsLeft();
+                }
+                for (int i = 0; i < walls.Count; i++)
+                {
+                    walls[i].Left += speeds[i];
+                }
+            }
+            if (rbUp.Checked)
+            {
+                bird.Left += (int)(gx.X * m * 2);
+                if (rnd.Next(12) == 1)
+                {
+                    CreateWallsUp();
+                }
+                for (int i = 0; i < walls.Count; i++)
+                {
+                    walls[i].Top += speeds[i];
+                }
+            }
+            if (rbDown.Checked)
+            {
+                bird.Left += (int)(gx.X * m * 2);
+                if (rnd.Next(12) == 1)
+                {
+                    CreateWallsDown();
+                }
+                for (int i = 0; i < walls.Count; i++)
+                {
+                    walls[i].Top -= speeds[i];
+                }
+            }
+            Collision();
+        }
+
+        //COLLISION AND FINISH
+
+        private void Collision()
+        {
+            for (int i = 0; i < walls.Count; i++)
+            {
+                if (bird.Bounds.IntersectsWith(walls[i].Bounds)|| bird.Bounds.IntersectsWith(hwall1.Bounds) 
+                    || bird.Bounds.IntersectsWith(hwall2.Bounds))
+                {
+                    timer.Stop();
+
+                    //FINISH MENU INFORMATION
+
+                    Panel pnlFinish = new Panel();
+                    pnlFinish.BackColor = Color.DarkOrchid;
+                    pnlFinish.Size = new Size(600, 400);
+                    pnlFinish.Location = new Point(this.ClientSize.Width/2-pnlFinish.Size.Width/2,this.ClientSize.Height/2-pnlFinish.Size.Height/2);
+                    Controls.Add(pnlFinish);
+                    pnlFinish.BringToFront();
+
+                    Button btnExit = new Button();
+                    btnExit.BackColor = Color.DarkViolet;
+                    btnExit.Size = new Size(150, 100);
+                    btnExit.Location = new Point(pnlFinish.Width / 2 - btnExit.Size.Width/2, pnlFinish.Height / 2 - btnExit.Size.Height/2);
+                    btnExit.Click += new EventHandler(btnExit_Click);
+                    btnStart.Name = "btnStart";
+                    btnExit.Text = "Exit";
+                    pnlFinish.Controls.Add(btnExit);
+                    btnExit.BringToFront();
+                }
+            }            
+        }
+
+        //FINISH BUTTON
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //KEYBOARD                   
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (rbLeft.Checked || rbRight.Checked)
+            {
+                if (e.KeyValue == (char)Keys.Enter)
+                {
+                    r0.Y += r.Y / 1.25;
+                    bird.Location = new Point((int)r0.X, (int)r0.Y);
+                }
+            }
+            if (rbUp.Checked || rbDown.Checked)
+            {
+                if (e.KeyValue == (char)Keys.Enter)
+                {
+                    r0.X += r.X / 1.25;
+                    bird.Location = new Point((int)r0.X, (int)r0.Y);
+                }
+            }
+        }
+
+        //BIRD INFORMATION
+
+        private void CreateBirdRight()
+        {
+            bird = new PictureBox();
+            bird.Size = new Size(70, 70);
+            bird.Location = new Point(20, this.ClientSize.Height / 2 - bird.Height / 2);
+            bird.ImageLocation = "Bird.png";
+            bird.SizeMode = PictureBoxSizeMode.Zoom;
+            Controls.Add(bird);
+            bird.BringToFront();
+        }
+        private void CreateBirdDown()
+        {
+            bird = new PictureBox();
+            bird.Size = new Size(70, 70);
+            bird.Location = new Point(this.ClientSize.Width / 2 - bird.Width / 2, 20 );
+            bird.ImageLocation = "Bird.png";
+            bird.SizeMode = PictureBoxSizeMode.Zoom;
+            Controls.Add(bird);
+            bird.BringToFront();
+        }
+        private void CreateBirdUp()
+        {
+            bird = new PictureBox();
+            bird.Size = new Size(70, 70);
+            bird.Location = new Point(this.ClientSize.Width / 2 - bird.Width / 2, ClientSize.Height-bird.Size.Height);
+            bird.ImageLocation = "Bird.png";
+            bird.SizeMode = PictureBoxSizeMode.Zoom;
+            Controls.Add(bird);
+            bird.BringToFront();
+        }
+        private void CreateBirdLeft()
+        {
+            bird = new PictureBox();
+            bird.Size = new Size(70, 70);
+            bird.Location = new Point(ClientSize.Width - bird.Size.Width, this.ClientSize.Height / 2 - bird.Height / 2);
+            bird.ImageLocation = "Bird.png";
+            bird.SizeMode = PictureBoxSizeMode.Zoom;
+            Controls.Add(bird);
+            bird.BringToFront();
+        }
+
+        //WALLS INFORMATION
+
+        private void CreateWallsDown()
+        {
+            int speedLimit = Convert.ToInt32(tbSpeed.Value);
+            PictureBox wallUp = new PictureBox();
+            wallUp.Size = new Size(rnd.Next(500, 550), 80);
+            wallUp.Location = new Point(0, (int)(ClientSize.Height * 1.3));
+            wallUp.ImageLocation = "Wall.jpg";
+            wallUp.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallUp);
+            walls.Add(wallUp);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            PictureBox wallDown = new PictureBox();
+            wallDown.Size = new Size(rnd.Next(500, 550), 80);
+            wallDown.Location = new Point(ClientSize.Width - wallUp.Size.Width, (int)(ClientSize.Height * 1.3));
+            wallDown.ImageLocation = "Wall.jpg";
+            wallDown.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallDown);
+            walls.Add(wallDown);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            wallUp.BringToFront();
+            wallDown.BringToFront();
+        }
+        private void CreateWallsUp()
+        {
+            int speedLimit = Convert.ToInt32(tbSpeed.Value);
+            PictureBox wallUp = new PictureBox();
+            wallUp.Size = new Size(rnd.Next(500, 550), 80);
+            wallUp.Location = new Point(0, (int)((ClientSize.Height - ClientSize.Height) / 1.3));
+            wallUp.ImageLocation = "Wall.jpg";
+            wallUp.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallUp);
+            walls.Add(wallUp);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            PictureBox wallDown = new PictureBox();
+            wallDown.Size = new Size(rnd.Next(500, 550), 80);
+            wallDown.Location = new Point(ClientSize.Width - wallUp.Size.Width, (int)((ClientSize.Height - ClientSize.Height) / 1.3));
+            wallDown.ImageLocation = "Wall.jpg";
+            wallDown.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallDown);
+            walls.Add(wallDown);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            wallUp.BringToFront();
+            wallDown.BringToFront();
+        }
+        private void CreateWallsLeft()
+        {
+            int speedLimit = Convert.ToInt32(tbSpeed.Value);
+            PictureBox wallUp = new PictureBox();
+            wallUp.Size = new Size(80, rnd.Next(200, 250));
+            wallUp.Location = new Point((int)((this.ClientSize.Width - this.ClientSize.Width) / 1.3), 0);
+            wallUp.ImageLocation = "Wall.jpg";
+            wallUp.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallUp);
+            walls.Add(wallUp);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            PictureBox wallDown = new PictureBox();
+            wallDown.Size = new Size(80, rnd.Next(200, 250));
+            wallDown.Location = new Point((int)((this.ClientSize.Width - this.ClientSize.Width) / 1.3), ClientSize.Height - wallDown.Size.Height);
+            wallDown.ImageLocation = "Wall.jpg";
+            wallDown.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallDown);
+            walls.Add(wallDown);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            wallUp.BringToFront();
+            wallDown.BringToFront();
+        }
+        private void CreateWallsRight()
+        {
+            int speedLimit = Convert.ToInt32(tbSpeed.Value);
+            PictureBox wallUp = new PictureBox();
+            wallUp.Size = new Size(80, rnd.Next(200, 250));
+            wallUp.Location = new Point((int)(this.ClientSize.Width * 1.3), ClientSize.Height - wallUp.Size.Height);
+            wallUp.ImageLocation = "Wall.jpg";
+            wallUp.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallUp);
+            walls.Add(wallUp);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            PictureBox wallDown = new PictureBox();
+            wallDown.Size = new Size(80, rnd.Next(200, 250));
+            wallDown.Location = new Point((int)(this.ClientSize.Width * 1.3), 0);
+            wallDown.ImageLocation = "Wall.jpg";
+            wallDown.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(wallDown);
+            walls.Add(wallDown);
+            speeds.Add(rnd.Next(tbSpeed.Minimum, speedLimit));
+
+            wallUp.BringToFront();
+            wallDown.BringToFront();
+        }
+
+        //INVISIBLE WALLS INFORMATION
+
+        private void CreateInvisibleWallsForUpAndDown()
+        {
+            hwall1 = new PictureBox();
+            hwall1.Size = new Size(1, ClientSize.Height);
+            hwall1.Location = new Point(-1, 0);
+            Controls.Add(hwall1);
+
+            hwall2 = new PictureBox();
+            hwall2.Size = new Size(1, ClientSize.Height);
+            hwall2.Location = new Point(ClientSize.Width, 0);
+            Controls.Add(hwall2);
+
+            hwall1.BringToFront();
+            hwall2.BringToFront();
+        }
+        private void CreateInvisibleWallsForLeftAndRight()
+        {
+            hwall1 = new PictureBox();
+            hwall1.Size = new Size(ClientSize.Width, 1);
+            hwall1.Location = new Point(0, -1);
+            Controls.Add(hwall1);
+
+            hwall2 = new PictureBox();
+            hwall2.Size = new Size(ClientSize.Width, 1);
+            hwall2.Location = new Point(0, ClientSize.Height);
+            Controls.Add(hwall2);
+
+            hwall1.BringToFront();
+            hwall2.BringToFront();
+        }
+    }
+}
